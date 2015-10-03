529305eb7fde4dc47c324f9064928c0adffdf8fc
DanVanAtta <dhvatta@gmail.com>
2015-10-02 22:58:04 -0700

    Initial add of ConquestMoveValidatorTest.java

---
+++ test/games/strategy/triplea/delegate/ConquestMoveValidatorTest.java
@@ -0,0 +1,18 @@
+package games.strategy.triplea.delegate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConquestMoveValidatorTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @Test
+  public void test() {
+      // TODO test
+  }
+
+}
