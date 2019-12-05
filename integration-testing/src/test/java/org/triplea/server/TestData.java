f3395e082c3e7479d94c970b42410fbeb515d11f
DanVanAtta <dhvatta@gmail.com>
2019-12-05 00:59:39 -0800

    wip

--- integration-testing/src/test/java/org/triplea/server/TestData.java
+++ integration-testing/src/test/java/org/triplea/server/TestData.java
@@ -6,18 +6,5 @@ import org.triplea.http.client.lobby.game.listing.LobbyGame;
 
 @UtilityClass
 public class TestData {
-  public static final LobbyGame LOBBY_GAME =
-      LobbyGame.builder()
-          .hostAddress("127.0.0.1")
-          .hostPort(12)
-          .hostName("name")
-          .mapName("map")
-          .playerCount(3)
-          .gameRound(1)
-          .epochMilliTimeStarted(Instant.now().toEpochMilli())
-          .mapVersion("1")
-          .passworded(false)
-          .status("Waiting For Players")
-          .comments("comments")
-          .build();
+
 }
